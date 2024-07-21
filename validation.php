2d0435d5d723befd2eeea57cf59964523dc257fc
midhun kalarikkal <91378594+midhunkalarikkal@users.noreply.github.com>
2024-07-21 16:15:02 +0530

    uiderror and validation.php edited

--- validation.php
+++ validation.php
@@ -1,16 +1,18 @@
 <?php
 ////// Function to generate OTP \\\\\\
-function generateOTP(){
+function generateOTP()
+{
     $digits = 6;
-    $otp = rand(pow(10, $digits-1), pow(10, $digits)-1);
+    $otp = rand(pow(10, $digits - 1), pow(10, $digits) - 1);
     return $otp;
 }
 
-function isEmailExists($email){
+function isEmailExists($email)
+{
     $conn = mysqli_connect("localhost", "root", "", "supplychain");
     $query = "SELECT * FROM authemail WHERE email='$email'";
     $result = mysqli_query($conn, $query);
-    if(mysqli_num_rows($result) > 0){
+    if (mysqli_num_rows($result) > 0) {
         return true;
     } else {
         return false;
@@ -25,58 +27,62 @@ use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\Exception;
 
 ////// function to send otp \\\\\\
-function sendOTP($email, $otp){
-   
+function sendOTP($email, $otp)
+{
+
     $mail = new PHPMailer(true);
-    
+
     try {
         ////// Server settings \\\\\\
         $mail->isSMTP();
-        $mail->Host       = 'smtp.gmail.com';
-        $mail->SMTPAuth   = true;
-        $mail->Username   = 'blckchainmedicine@gmail.com';
-        $mail->Password   = 'shggbhblxfkmvbbo';
+        $mail->Host = 'smtp.gmail.com';
+        $mail->SMTPAuth = true;
+        $mail->Username = 'blckchainmedicine@gmail.com';
+        $mail->Password = 'shggbhblxfkmvbbo';
         $mail->SMTPSecure = 'tls';
-        $mail->Port       = 587;
-    
+        $mail->Port = 587;
+
         ////// Sender, recipient details and content \\\\\\
         $mail->setFrom('blckchainmedicine@gmail.com', 'MEDICARE');
         $mail->addAddress($email, 'User');
         $mail->isHTML(true);
         $mail->Subject = 'OTP';
-        $mail->Body    = 'This is your otp '.$otp ."<br> <br> MEDICARE is a Decentralized E2E Application that stores the whereabouts of MEDICINE at every freight hub on the Blockchain. At consumer end, customers can simply scan the QR CODE of products and get complete information about the provenance of that product hence empowering consumers to only purchase authentic and quality products.";
-    
+        $mail->Body = 'This is your otp ' . $otp . "<br> <br> MEDICARE is a Decentralized E2E Application that stores the whereabouts of MEDICINE at every freight hub on the Blockchain. At consumer end, customers can simply scan the QR CODE of products and get complete information about the provenance of that product hence empowering consumers to only purchase authentic and quality products.";
+
         $mail->send();
         echo 'Email sent successfully!';
-        } catch (Exception $e) {
+    } catch (Exception $e) {
         echo 'Error: ' . $mail->ErrorInfo;
     }
-    
+
 }
 
 ////// Function to save OTP to the database \\\\\\
-function saveOTP($email, $otp){
+function saveOTP($email, $otp)
+{
     $conn = mysqli_connect("localhost", "root", "", "supplychain");
     $query = "INSERT INTO authemailotp (email, otp) VALUES ('$email', '$otp')";
     mysqli_query($conn, $query);
 }
 
-function saveEmailAndOTPToSession($email){
+function saveEmailAndOTPToSession($email)
+{
     $_SESSION['temp_email'] = $email;
 }
 
 ////// Function to delete email and otp after one minute from database \\\\\\
-function deleteExpiredEntries(){
+function deleteExpiredEntries()
+{
     $conn = mysqli_connect("localhost", "root", "", "supplychain");
     $query = "DELETE FROM authemailotp WHERE created_at < NOW() - INTERVAL 1 MINUTE";
     mysqli_query($conn, $query);
 }
 
 ////// Check if form is submitted \\\\\\
-if(isset($_POST['submit'])){
+if (isset($_POST['submit'])) {
     $email = $_POST['email'];
-    
-    if(isEmailExists($email)){
+
+    if (isEmailExists($email)) {
         $otp = generateOTP();
         saveOTP($email, $otp);
         // Save the email and OTP to the session
@@ -86,7 +92,7 @@ if(isset($_POST['submit'])){
 
         $variable = $_SESSION['temp_email'];
         $url = "otp.php?var=" . urlencode($variable);
-        header("Location: " . $url);        
+        header("Location: " . $url);
     } else {
         echo "Email does not exist in the database.";
     }
@@ -95,4 +101,3 @@ if(isset($_POST['submit'])){
 ////// Delete expired email and OTP entries \\\\\\
 deleteExpiredEntries();
 ?>
-
